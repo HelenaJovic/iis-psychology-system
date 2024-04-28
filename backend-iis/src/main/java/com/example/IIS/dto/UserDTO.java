36d53ddfc28d6b35b62b952c531050ceb50328c8
Helena <h.jovix@gmail.com>
2024-04-28 15:16:26 +0200

    Finished user and psychologist overview

--- backend-iis/src/main/java/com/example/IIS/dto/UserDTO.java
+++ backend-iis/src/main/java/com/example/IIS/dto/UserDTO.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
+    private Long id;
     private String name;
     private String lastName;
     private String username;
